493a447cfdf09f929d84df0fcdbfd951d03134c8
Mr_y <birdfly551@live.com>
2019-02-22 17:52:30 +0800

    none

--- Vote/client/GccVoteClientTCP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -g *.c ../src/*.c -o ../../../bin/VoteClientTCP.out -I ../src
-./../../../bin/VoteClientTCP.out $*
--- Vote/server/GccVoteServerTCP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc -g *.c ../src/*.c -o ../../../bin/VoteServerTCP.out -I ../src
-#gcc -g TCPEchoClient.c Deal_Error.c -o ../../bin/TCPEchoClient.out
-#cd ../../bin
-./../../../bin/VoteServerTCP.out $*
